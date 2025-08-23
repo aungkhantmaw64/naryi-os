85d9db5cdf0b41e950d2106b27dac1130119cf6f
Aung Khant Maw <aungkhantmaw64@gmail.com>
2025-08-23 14:56:10 +0700

    scripts: init west properly

--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -4,14 +4,10 @@ source ${ZEPHYR_ROOT}/.venv/bin/activate
 
 if [ ! -d ".west" ]; then
   echo "Initilizing west for the first time"
+  rm -rf ${ZEPHYR_ROOT}/.west
   west init -l naryi-os
   west update
   west zephyr-export
-  west packages pip --install
-  pushd zephyr
-  echo "Install zephyr sdk with west"
-  west sdk install
-  popd
 fi
 
 echo "Zephyr Project dev environment is ready!"
